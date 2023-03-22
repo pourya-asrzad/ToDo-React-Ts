faefa2d0730f99d0bd8e9d08514dfcb9fe8ba584
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-03-22 23:27:28 +0430

    fix import bug in card

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,17 +1,17 @@
-import './card.component.scss';
+import styles from'./card.module.scss';
 const Card=()=>{
     return<>
-    <div className='card-container'>
-        <div className='card-detail'>
-            <div className='card-header'>
-                <div className='card-title'>Todo Name</div>
-                <span className='card-todo-color'></span>
+    <div className={styles['card-container']}>
+        <div className={styles['card-detail']}>
+            <div className={styles['card-header']}>
+                <div className={styles['card-title']}>Todo Name</div>
+                <span className={styles['card-todo-color']}></span>
             </div>
-            <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit atque perspiciatis voluptas<button className='card-text-option'>more...</button></p>
+            <p className={styles['card-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit atque perspiciatis voluptas<button className={styles['card-text-option']}>more...</button></p>
         </div>
-        <div className='card-buttons'>
-            <button className='card-button'>Edit</button>
-            <button className='card-button'>Delete</button>
+        <div className={styles['card-buttons']}>
+            <button className={styles['card-button']}>Edit</button>
+            <button className={styles['card-button']}>Delete</button>
         </div>
     </div>
     </>
