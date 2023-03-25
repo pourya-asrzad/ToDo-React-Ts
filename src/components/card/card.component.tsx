cf8cc70f83f1794262aea518ade930f9b57fd464
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-03-26 00:48:41 +0430

    fix card bugs

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,19 +1,46 @@
+import { useState } from 'react';
 import styles from'./card.module.scss';
-const Card=()=>{
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const Cart=()=>{
+    const [toggelBox,setToggeleBox]=useState(false);
+    const moreButton = ()=>{
+        setToggeleBox(true);
+    }
+    const lessButton =()=>{
+        setToggeleBox(false)
+    }
+    const bull = (
+        <Box
+          component="span"
+          sx={{ color:'red', display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+        >
+          •
+        </Box>
+      );
     return<>
-    <div className={styles['card-container']}>
+    <Card  className={!toggelBox ? styles['card-container'] : styles['card-container-toggle']}>
+        <CardContent  >
+        <Typography  className={styles['card-title']} component="div">
+            Card Titel { bull}
+        </Typography>
         <div className={styles['card-detail']}>
-            <div className={styles['card-header']}>
-                <div className={styles['card-title']}>Todo Name</div>
-                <span className={styles['card-todo-color']}></span>
+               <div className={styles['card-paragragh']}> <p className={!toggelBox ? styles['card-text'] : styles['card-text-toggle']}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, voluptates. Tempore sit officia repudiandae illum libero! Ut eos, sed dicta architecto ex iusto placeat provident facilis similique molestiae consectetupkokpmomjhhbnmquisquamaljkdfhlkasdjfsldkjfafjkdhfskjfd?</p>
+                    <div className={styles['text-button']}>
+                        <span onClick={ moreButton} className={!toggelBox ?styles['card-text-option']:styles['hidden'] }>more</span>
+                        <span onClick={ lessButton } className={toggelBox ?styles['card-text-option'] : styles['hidden']}>less</span>
+                    </div>
+                </div>
+            <div className={styles['card-buttons']}>
+                <button className={styles['card-button']}>Edit</button>
+                <button className={styles['card-button']}>Delete</button>
             </div>
-            <p className={styles['card-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit atque perspiciatis voluptas<button className={styles['card-text-option']}>more...</button></p>
         </div>
-        <div className={styles['card-buttons']}>
-            <button className={styles['card-button']}>Edit</button>
-            <button className={styles['card-button']}>Delete</button>
-        </div>
-    </div>
+        </CardContent>
+    </Card>
     </>
 }
-export default Card;
+export default Cart;
