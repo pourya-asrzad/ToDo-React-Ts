e3c43385abf02ea063f124e36fa94db8139221e4
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-27 14:25:59 +0430

    Arman-Kangavari/task8&7/medium/ modalDelete/complete

--- src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
@@ -1,6 +1,18 @@
 import Cart from "../../../card/card.component";
+import CardModal from "./cardModal/cardModal.component";
+import React from "react";
 
 const ListContainer = () => {
-    return <Cart />
+  const [modalDelete, setModalDelete] = React.useState<boolean>(false);
+  return (
+    <>
+      <Cart setterDelete={setModalDelete} state={modalDelete} />
+      <CardModal
+        visibility={true}
+        state={modalDelete}
+        modalSetter={setModalDelete}
+      />
+    </>
+  );
 };
 export default ListContainer;
