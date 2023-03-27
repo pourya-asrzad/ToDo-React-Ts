9bbb48fa03f1643c1b58d0c0700aaa789145727e
Pouria Asrzad <poriaasrzad@gmail.com>
2023-03-28 01:07:54 +0330

    api added

--- src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
@@ -1,10 +1,12 @@
 import Cart from "../../../card/card.component";
 import CardModal from "./cardModal/cardModal.component";
 import React from "react";
-
+import useGetTodosQuery from "?"
 const ListContainer = () => {
+const { data: todos = [], isLoading: someProductLoading, isSuccess } = useGetTodosQuery()
   const [modalDelete, setModalDelete] = React.useState<boolean>(false);
-  return (
+ 
+ return (
     <>
       <Cart setterDelete={setModalDelete} state={modalDelete} />
       <CardModal
