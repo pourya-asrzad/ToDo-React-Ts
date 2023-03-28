9129522ffe7a2ea6fed0c3ad66e8c9d9385d0795
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-28 23:04:17 +0430

    Pouria-asrzad/task10:medium/compeleted/refactored

--- src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
@@ -1,12 +1,9 @@
-import Cart from "../../../card/card.component";
 import CardModal from "./cardModal/cardModal.component";
 import React from "react";
-import { useGetTodosQuery } from "../../../../features/api/apiSlice";
+
 import RenderCards from "./renderingCard";
 const ListContainer = () => {
   const [modalDelete, setModalDelete] = React.useState<boolean>(false);
-  const { data: Todos, isSuccess } = useGetTodosQuery();
-  console.log(Todos);
 
   const content = RenderCards();
 
