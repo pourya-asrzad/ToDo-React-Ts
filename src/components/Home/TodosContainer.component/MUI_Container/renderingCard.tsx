844d69ae97170a304dc76793529d936b3f88801c
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-28 23:35:03 +0430

    Arman-Kangavari/newSpinnerAdded

--- src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
@@ -1,12 +1,17 @@
 import { useGetTodosQuery } from "../../../../features/api/apiSlice";
 import Cart from "../../../card/card.component";
 import React, { ReactElement } from "react";
+import Styles from "./spinner/sppiner.module.scss";
 const RenderCards = (): ReactElement | ReactElement[] => {
   const [modalDelete, setModalDelete] = React.useState<boolean>(false);
   const { data: Todos, isSuccess } = useGetTodosQuery();
 
   return !isSuccess ? (
-    <span>Loading</span>
+    <>
+      {" "}
+      <div className={Styles["inner-circle"]}></div>
+      <div className={Styles["outer-Circle"]}></div>
+    </>
   ) : (
     Todos.map((item: any, i: number) => {
       return (
