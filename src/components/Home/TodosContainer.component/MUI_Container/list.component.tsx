6e923b33a83555906a6d61da933d1a27993c190e
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-28 22:51:41 +0430

    Pouriya-Asrzad/task10:medium /rendering/card/compelete

--- src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/list.component.tsx
@@ -1,12 +1,25 @@
 import Cart from "../../../card/card.component";
 import CardModal from "./cardModal/cardModal.component";
 import React from "react";
-
+import { useGetTodosQuery } from "../../../../features/api/apiSlice";
 const ListContainer = () => {
   const [modalDelete, setModalDelete] = React.useState<boolean>(false);
+  const { data: Todos, isSuccess } = useGetTodosQuery();
+  console.log(Todos);
+
+  const content = !isSuccess ? (
+    <span>Loading</span>
+  ) : (
+    Todos.map((item: any, i: number) => {
+      return (
+        <Cart setterDelete={setModalDelete} state={modalDelete} key={item[i]} />
+      );
+    })
+  );
+
   return (
     <>
-      <Cart setterDelete={setModalDelete} state={modalDelete} />
+      <div className="">{content}</div>
       <CardModal
         visibility={true}
         state={modalDelete}
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,3 @@
 declare module "*.scss";
 declare module "@reduxjs/toolkit/query/react";
+declare module "@reduxjs/toolkit";
