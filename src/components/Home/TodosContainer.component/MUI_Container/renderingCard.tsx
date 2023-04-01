bdf75f5834886228b420dab7043eef5630c4fbc4
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-04-01 18:07:57 +0330

    add limit to API_LINK

--- src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
@@ -2,10 +2,20 @@ import { useGetTodosQuery } from "../../../../features/api/apiSlice";
 import Cart from "../../../card/card.component";
 import React, { ReactElement } from "react";
 import Styles from "./spinner/sppiner.module.scss";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { API_LINK } from "../../../../api/apis";
 const RenderCards = (): ReactElement | ReactElement[] => {
   const [modalDelete, setModalDelete] = React.useState<boolean>(false);
   const { data: Todos, isSuccess } = useGetTodosQuery();
-
+  const [items,setItems] = React.useState(Todos)
+  const data = React.useMemo(() => items, [items]);
+  const fetchMoreData=()=>{
+    setTimeout(()=>{
+      setItems(items.concat(Todos(2)))
+    },1500)
+  }
+  console.log(API_LINK);
+  
   return !isSuccess ? (
     <>
       {" "}
@@ -13,11 +23,19 @@ const RenderCards = (): ReactElement | ReactElement[] => {
       <div className={Styles["outer-Circle"]}></div>
     </>
   ) : (
-    Todos.map((item: any, i: number) => {
+  //   <InfiniteScroll
+  //   dataLength={data}
+  //   next={()=>fetchMoreData}
+  //   hasMore={true}
+  //   loader={<h4>Loading...</h4>}
+  // >
+     Todos.map((item: any, i: number) => {
       return (
         <Cart setterDelete={setModalDelete} state={modalDelete} key={item.id} />
       );
     })
+  // </InfiniteScroll>
+ 
   );
 };
 
