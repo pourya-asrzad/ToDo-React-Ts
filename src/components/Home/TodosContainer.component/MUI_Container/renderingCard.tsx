df04e9efd8331f609b43134b9797f1f81a557d99
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-04-07 23:37:54 +0330

    Ahmad-Movahedi/fix import bug

--- src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
+++ src/components/Home/TodosContainer.component/MUI_Container/renderingCard.tsx
@@ -1,5 +1,5 @@
 import ErrorImage from "../../../../assets/images/404.jpg";
-// import Styles from "./todocontainer.module.scss";
+import Styles from "./todocontainer.module.scss";
 import NoConnection from "../../../../assets/images/noconnection.avif";
 import {
   useGetTodosQuery,
@@ -8,56 +8,36 @@ import {
   useFetchTodoLengthQuery,
 } from "../../../../features/api/apiSlice";
 import Cart from "../../../card/card.component";
-import React, { ReactElement, useEffect, useState } from "react";
-import Styles from "./spinner/sppiner.module.scss";
+import { ReactElement, useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { CircularProgress } from "@mui/material";
 import SkeletonComponent from "../../../card/skeleton.component";
-
+import { fakeArray, userOnline } from "../../../../utils/index.utils";
+//////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 const RenderCards = (): ReactElement | ReactElement[] => {
   const [modalDelete, setModalDelete] = useState<boolean>(false);
-  const { data: Todos, isSuccess, isLoading } = useGetTodosQuery();
   const [items, setItems] = useState<any>(null);
   const [hasMore, setHasMore] = useState<boolean>(true);
   const [page, setPage] = useState<number>(2);
-
-  const LIMIT_LINK: any = new URL(
-    "https://6347eca8db76843976b5e973.mockapi.io/todos"
-  );
-
-  // useEffect(() => {
-  //   console.log("object");
-  // }, []);
-  let initialized = false;
-
+  const {
+    data: firstData,
+    isSuccess,
+    isLoading,
+    isError: errorInFirstData,
+  } = useGetDataQuery();
+  const { data: newData, isError: errorInNewData } = useGetNewDataQuery(page);
+  const { data: dataLength } = useFetchTodoLengthQuery();
   useEffect(() => {
-    if (!initialized) {
-      const getData = async () => {
-        LIMIT_LINK.searchParams.append("limit", 5);
-        LIMIT_LINK.searchParams.append("page", 1);
-        const res = await fetch(LIMIT_LINK);
-        const data = await res.json();
-        console.log(data.length > 0 ? "i have data" : " i don't have data");
-        console.log(data);
-        setItems(data);
-      };
-      getData();
-      initialized = true;
+    if (isSuccess) {
+      setItems(firstData);
     }
-  }, []);
+  }, [isSuccess]);
 
-  const fetchNewData = async () => {
-    LIMIT_LINK.searchParams.append("limit", 10);
-    LIMIT_LINK.searchParams.append("page", `${page}`);
-    const res = await fetch(LIMIT_LINK);
-    const data = await res.json();
-    return data;
-  };
+  //////////////////////////////////
   const fetchMoreData = async () => {
-    const newData = await fetchNewData();
-    console.log([...items, ...newData]);
     setItems([...items, ...newData]);
-    if (newData.length === 0 || newData.length < 10) {
+    if (items.length + newData.length === dataLength) {
       setHasMore(false);
     }
     setPage(page + 1);
@@ -78,7 +58,6 @@ const RenderCards = (): ReactElement | ReactElement[] => {
       {items.map((item: any) => {
         return (
           <>
-            <SkeletonComponent />
             <Cart
               setterDelete={setModalDelete}
               state={modalDelete}
